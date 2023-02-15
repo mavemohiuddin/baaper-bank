7e5b7805672e733a2548b923fb4978404700398b
M. Hasan <mohiuddinkhasherhat@gmail.com>
2023-02-15 15:01:10 +0600

    final update

--- script.js
+++ script.js
@@ -5,18 +5,24 @@ let mainScreen = document.querySelector("account");
 
 // =============================== Input field animation ===============================
 
+const set_label = item => {
+    item.children[0].classList.add("top-0");
+    item.children[0].classList.add("text-xs");
+}
+const reset_label = item => {
+    if (item.children[1].value == "") {
+        item.children[0].classList.remove("top-0");
+        item.children[0].classList.remove("text-xs");
+    }
+}
 document.querySelectorAll(".input_field").forEach((item) => {
     item.children[1].addEventListener("focus", () => {
-        item.children[0].classList.add("top-0");
-        item.children[0].classList.add("text-xs");
+        set_label(item);
     })
 })
 document.querySelectorAll(".input_field").forEach((item) => {
     item.children[1].addEventListener("blur", () => {
-        if (item.children[1].value == "") {
-            item.children[0].classList.remove("top-0");
-            item.children[0].classList.remove("text-xs");
-        }
+        reset_label(item);
     })
 })
 
@@ -36,16 +42,27 @@ document.querySelectorAll("[data-object='block']").forEach((item) => {
     item.addEventListener("click", () => {
         if (item.children[1].getAttribute("data-state") == "closed") {
             document.querySelectorAll("[data-object='block']").forEach((item) => {
+                item.classList.remove("bg-orange-100");
+                if (item.querySelector("input") != null) {
+                    item.querySelector("input").value = "";
+                }
                 item.children[1].setAttribute("data-state", "closed");
                 switch_state(item, "add", ["h-0", "w-0", "pb-0", "opacity-0", "pointer-events-none"]);
             })
             item.children[1].setAttribute("data-state", "open");
+            item.classList.add("bg-orange-100");
             switch_state(item, "remove", ["h-0", "w-0", "pb-0", "opacity-0", "pointer-events-none"]);
         } else {
             item.children[1].setAttribute("data-state", "closed");
+            item.classList.remove("bg-orange-100");
             switch_state(item, "add", ["h-0", "w-0", "pb-0", "opacity-0", "pointer-events-none"]);
         }
     })
+})
+
+document.querySelector("#logout").addEventListener("click", () => {
+    mainScreen.classList.add("hidden");
+    load("login");
 })
 
 const switch_state = (item, state, arr) => {
@@ -66,12 +83,13 @@ document.querySelectorAll("[data-object='interaction']").forEach((item) => {
     })
 })
 
-// Money interactions
+// =================================== Money interactions ======================================
 
 document.querySelector("#deposit_submit").addEventListener("click", (event) => {
     let deposit_amount = event.target.parentElement.children[0].value;
     let balance = document.querySelector("#balance");
     let change = document.querySelector("#change_plus");
+    let currentUser = document.querySelector("#user").innerHTML;
     change.innerHTML = `+$${deposit_amount}`;
     change.classList.add("opacity-100");
     change.classList.add("translate-y-0");
@@ -80,10 +98,16 @@ document.querySelector("#deposit_submit").addEventListener("click", (event) => {
         change.classList.remove("opacity-100");
         change.classList.remove("translate-y-0");
         balance.innerHTML = parseInt(balance.innerHTML) + parseInt(deposit_amount);
+        for (i in user.all_users) {
+            if (currentUser == user.all_users[i].username) {
+                user.all_users[i].amount = parseInt(balance.innerHTML);
+            }
+        }
     }, 1000)
     setTimeout(() => {
         change.classList.remove("hidden");
     }, 1100)
+    event.target.parentElement.children[0].value = "";
 })
 
 document.querySelector("#withdraw_submit").addEventListener("click", (event) => {
@@ -105,10 +129,11 @@ document.querySelector("#withdraw_submit").addEventListener("click", (event) => 
         setTimeout(() => {
             change.classList.add("hidden");
         }, 850)
+        event.target.parentElement.children[0].value = "";
     }
 })
 
-// Swithcing between screens
+// =================================== Swithcing between screens ======================================
 
 loginScreen.querySelector("[ data-type='alt']").addEventListener("click", () => {
     load("signup");
@@ -119,6 +144,9 @@ signupScreen.querySelector("[ data-type='alt']").addEventListener("click", () =>
 })
 
 const load = input => {
+    document.querySelectorAll(".error_message").forEach(item => item.innerHTML = "");
+    document.querySelectorAll("input").forEach(item => item.value = "");
+    document.querySelectorAll(".input_field").forEach((item) => reset_label(item));
     loadingScreen.classList.remove("hidden");
     if (input == "signup") {
         setTimeout(() => {
@@ -133,7 +161,9 @@ const load = input => {
             loginScreen.classList.remove("hidden");
         }, 180)
     } else if (input == "main") {
+        populate_main();
         setTimeout(() => {
+            signupScreen.classList.add("hidden");
             loginScreen.classList.add("hidden");
             loadingScreen.classList.add("hidden");
             mainScreen.classList.remove("hidden");
@@ -141,12 +171,13 @@ const load = input => {
     }
 }
 
-// User Database
+// ============================================ User Database ===========================================
 
 class user {
-    constructor(username, code) {
+    constructor(username, code, amount) {
         this.username = username;
         this.code = code;
+        this.amount = amount;
         user.addMe(this);
     }
 
@@ -157,10 +188,10 @@ class user {
     static all_users = [];
 }
 
-const admin = new user("admin", "admin");
-const guest = new user("guest", "guest");
+const admin = new user("admin", "admin", "30000");
+const guest = new user("guest", "guest", "100");
 
-// Signing up
+// ========================================== Signing up ===========================================
 
 signupScreen.querySelector("[ data-role='submit']").addEventListener("click", () => {
     let newName = signupScreen.querySelector("#signup_id");
@@ -183,13 +214,19 @@ signupScreen.querySelector("[ data-role='submit']").addEventListener("click", ()
             errorMessage.innerHTML = "Password must contain at least 8 digit, 1 uppercase, and 1 lowercase";
         } else {
             newPass.parentElement.classList.remove("border-red-400");
-            let newUser = new user(newName.value, newPass.value);
+            let newUser = new user(newName.value, newPass.value, "1200");
             load("login");
         }
     }
 })
 
-// Logging in
+signupScreen.querySelector("[data-role='assist'][data-object='guest']").addEventListener("click", () => {
+    let currentUser = document.querySelector("#user");
+    currentUser.innerHTML = "guest";
+    load("main");
+})
+
+// ======================================== Logging in ============================================
 
 loginScreen.querySelector("[ data-role='submit']").addEventListener("click", () => {
     let newName = loginScreen.querySelector("#login_id");
@@ -200,8 +237,8 @@ loginScreen.querySelector("[ data-role='submit']").addEventListener("click", () 
     for (i in user.all_users) {
         if (newName.value == user.all_users[i].username) {
             if (newPass.value == user.all_users[i].code) {
-                load("main");
                 currentUser.innerHTML = user.all_users[i].username;
+                load("main");
             } else {
                 newName.parentElement.classList.remove("border-red-400");
                 newPass.parentElement.classList.add("border-red-400");
@@ -215,3 +252,32 @@ loginScreen.querySelector("[ data-role='submit']").addEventListener("click", () 
         }
     }
 })
+
+loginScreen.querySelector("[data-object='forgot']").addEventListener("click", () => {
+    let newName = loginScreen.querySelector("#login_id");
+    let errorMessage = loginScreen.querySelector(".error_message");
+    for (i in user.all_users) {
+        if (newName.value == user.all_users[i].username) {
+            errorMessage.innerHTML = `Hello ${user.all_users[i].username}, Your password is: ${user.all_users[i].code}.`;
+            break;
+        } else {
+            errorMessage.innerHTML = "Please enter a correct username";
+        }
+    }
+})
+
+// =================================== Populating main screen ==================================
+
+const populate_main = () => {
+    let currentUser = mainScreen.querySelector("#user");
+    let balance = mainScreen.querySelector("#balance");
+
+    for (i in user.all_users) {
+        if (currentUser.innerHTML == user.all_users[i].username) {
+            console.log(user.all_users[i]);
+            balance.innerHTML = user.all_users[i].amount;
+        } else {
+            console.log(user.all_users[i]);
+        }
+    }
+}
